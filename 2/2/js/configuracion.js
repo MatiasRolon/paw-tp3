aeea59e5f98a9261237138500266c500811ec500
MaxiBrizuela <48636251+MaxiBrizuela@users.noreply.github.com>
2019-04-18 21:39:26 -0300

    correcciones tateti.js

---
+++ 2/2/js/configuracion.js
@@ -0,0 +1,24 @@
+var console = console || {},
+Juego = Juego || {};
+
+//Juego.imagenes = ["circulo","equis"];
+
+Juego.niveles = [
+  {
+    ancho: 3,
+    alto: 3
+  },
+  {
+    ancho: 4,
+    alto: 4
+  },
+  {
+    ancho: 5,
+    alto: 5
+  },
+    {
+    ancho: 6,
+    alto: 6
+  }
+  
+];
